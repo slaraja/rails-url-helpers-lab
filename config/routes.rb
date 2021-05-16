507ecefa3edee4552c1061a5629415e98715199d
Stephanie La Raja <stephanielaraja@gmail.com>
2021-05-16 14:25:31 -0400

    commit tree 5776a3d0bd813bbd2fea15ba4ecd76cad2ca4e7d

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'students', to: 'students#index'
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
+
+  # get 'students', to: 'students#index'
+  resources :students, only: [:index, :show]
 
 end
---
+++ db/migrate/20210516165437_add_column.rb
@@ -0,0 +1,6 @@
+class AddColumn < ActiveRecord::Migration[5.0]
+  def change
+    add_column :students, :active, :boolean, default: false
+
+  end
+end
